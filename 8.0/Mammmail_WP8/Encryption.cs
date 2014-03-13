0770aafc9c1e77c0377d852b202b6e68ec526104
Abhishek De <abhishekde@hotmail.com>
2014-03-13 20:03:20 +0530

    Added encryption class

---
+++ 8.0/Mammmail_WP8/Encryption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammmail_WP8
+{
+    public class Encryption
+    {
+        
+    }
+}
